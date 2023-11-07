6cd151dc8811097f49439b42f402258946c64d1d
GARANCINI <clement.garancini@orange.fr>
2023-11-07 11:28:02 -0500

    20231107: Double Jump

--- Mario/Assets/Scripts/PlayerMovement.cs
+++ Mario/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,37 @@
-
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private new Rigidbody2D rigidbody;
-    
+
+    // Camera
     private new Camera camera;
+
+    // Componetns
+    private new Rigidbody2D rigidbody;
     private new Collider2D collider;
 
-    private float inputAxis;
-    private Vector2 velocity;
-
+    // Player Characteristics
+    //// Speed and movement
     public float moveSpeed = 8f;
+    //// Jump
     public float maxJumpHeight = 5f;
-    public float maxJumpTime = 1f;
-
-    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
-    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2);
-
-    public bool grounded {get; private set;}
-    public bool jumping {get; private set;}
+    public float maxJumpTime = 2f;
+    public float jumpForce => 4f * maxJumpHeight;
+    private float jumpDone;
+    //// Gravity
+    public float gravity => -8f * maxJumpHeight;
+    //// World state
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
     public bool sliding => (inputAxis > 0f && velocity.x < 0f) || (inputAxis < 0f && velocity.x > 0f);
     public bool running => Mathf.Abs(velocity.x) > 0.25f || Mathf.Abs(inputAxis) > 0.25f;
+    private Vector2 velocity;
 
-    
-    
+    // Input
+    private float inputAxis;
+
+
 
 
     private void Awake()
@@ -39,7 +46,8 @@ public class PlayerMovement : MonoBehaviour
         collider.enabled = true;
         rigidbody.isKinematic = false;
         velocity = Vector2.zero;
-        jumping = false;    
+        jumping = false;
+        jumpDone = 0f;
     }
 
     private void OnDisable()
@@ -52,14 +60,15 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update()
-    {   
+    {
         HorizontalMovement();
-        grounded = rigidbody.Raycast(Vector2.down);
-        if(grounded){
-            GroundedMovement();
+        VerticalMovement();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
         }
 
-        ApplyGravity();
     }
 
     private void HorizontalMovement()
@@ -67,36 +76,50 @@ public class PlayerMovement : MonoBehaviour
         inputAxis = Input.GetAxis("Horizontal");
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, 2 * moveSpeed * Time.deltaTime);
 
-        if(rigidbody.Raycast(Vector2.right * velocity.x)){
+        if (rigidbody.Raycast(Vector2.right * velocity.x))
+        {
             velocity.x = 0f;
         }
 
-        if(velocity.x >0f){
+        if (velocity.x > 0f)
+        {
             transform.eulerAngles = Vector3.zero;
         }
-        else if (velocity.x < 0f){
+        else if (velocity.x < 0f)
+        {
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-        
+
     }
 
-    private void GroundedMovement()
+    private void VerticalMovement()
     {
-        velocity.y = Mathf.Max(velocity.y, 0f);
-        jumping = velocity.y > 0f;
-        if(Input.GetButtonDown("Jump"))
-        {
-            velocity.y = jumpForce;
-            jumping = true;
-        }
-    }
+        //grounded = rigidbody.Raycast(Vector2.down);
 
-    private void ApplyGravity()
-    {
         bool falling = velocity.y < 0f || !Input.GetButton("Jump");
         float multiplier = falling ? 2f : 1f;
         velocity.y += multiplier * gravity * Time.deltaTime;
         velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+        if (rigidbody.Raycast(Vector2.down))
+        {
+            velocity.y = Mathf.Max(velocity.y, 0f);
+            jumping = velocity.y > 0f;
+        }
+    }
+
+    private void Jump()
+    {
+        grounded = rigidbody.Raycast(Vector2.down);
+        if (grounded) { jumpDone = 0; }
+
+        if (jumpDone < maxJumpTime)
+        {
+            jumpDone++;
+            velocity.y = jumpForce;
+            jumping = true;
+            grounded = false;
+        }
+
     }
 
     private void FixedUpdate()
@@ -113,21 +136,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            if(transform.DotTest(collision.transform, Vector2.down))
+            if (transform.DotTest(collision.transform, Vector2.down))
             {
                 velocity.y = jumpForce / 2f;
                 jumping = true;
             }
         }
 
-        else if(collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        else if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
         {
-            if(transform.DotTest(collision.transform, Vector2.up)){
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
                 velocity.y = 0f;
             }
-        } 
+        }
     }
 
 }
